7869ebfe6958c7ba31cafab9ad5a0f2e4a01e0f8
Chad Scherrer <chad.scherrer@gmail.com>
2021-04-25 08:57:35 -0700

    formatting

--- src/chainvec.jl
+++ src/chainvec.jl
@@ -11,32 +11,30 @@ to information stored in a single sample.
 """
 function chainvec end
 
-
-function chainvec(nt::NamedTuple, n=1)
-tv = TupleVector(undef, nt, n)
-@inbounds tv[1] = nt
-return tv
+function chainvec(nt::NamedTuple, n = 1)
+    tv = TupleVector(undef, nt, n)
+    @inbounds tv[1] = nt
+    return tv
 end
 
-function chainvec(x::T, n=1) where {T<:Real}
-ev = ElasticVector{T}(undef, n)
-@inbounds ev[1] = x
-return ev
+function chainvec(x::T, n = 1) where {T<:Real}
+    ev = ElasticVector{T}(undef, n)
+    @inbounds ev[1] = x
+    return ev
 end
 
-function chainvec(x::T, n=1) where {T}
-if isstructtype(T)
-    v = StructArrays.replace_storage(ElasticVector, StructVector{T}(undef, n))
-    @inbounds v[1] = x
-    return v
-end
-ev = ElasticVector{T}(undef, n)
-@inbounds ev[1] = x
-return ev
+function chainvec(x::T, n = 1) where {T}
+    if isstructtype(T)
+        v = StructArrays.replace_storage(ElasticVector, StructVector{T}(undef, n))
+        @inbounds v[1] = x
+        return v
+    end
+    @inbounds ev[1] = x
+    return ev
 end
 
-function chainvec(x::DenseArray{T,N}, n=1) where {T,N}
-nv = nestedview(ElasticArray{T,N+1}(undef, size(x)..., n), N)
-@inbounds nv[1] = x
-return nv
-end
+function chainvec(x::DenseArray{T,N}, n = 1) where {T,N}
+    nv = nestedview(ElasticArray{T,N + 1}(undef, size(x)..., n), N)
+    @inbounds nv[1] = x
+    return nv
+end 
