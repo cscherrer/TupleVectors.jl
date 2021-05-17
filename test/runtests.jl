40aa7db7fc65d7651af7405dd661f9f9ff921966
Chad Scherrer <chad.scherrer@gmail.com>
2021-05-17 10:27:57 -0700

    tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,43 @@
 using TupleVectors
+using Sobol
 using Test
+using TupleVectors: chainvec, unwrap
+using MeasureTheory
+using LinearAlgebra
 
 @testset "TupleVectors.jl" begin
-    # Write your tests here.
+    @test chainvec(3,5)[1] == 3
+
+
+    x = randn(2,2)
+    A = chainvec((x = x,),100);
+
+    @test A.x[1] == x
+    @test A[1].x == A.x[1]
+
+    for i in 2:100
+        A[i] = (x=randn(2,2),)
+    end
+
+    B = @with A begin
+        (x = x' + x,)
+    end
+
+    C = @with B begin
+        (eigs = eigvals!(x),)
+    end
+
+    using Sobol
+
+    ω = SobolHypercube(2)
+
+    πapprox = @with (;ω) 10000 begin
+        x = rand(ω)
+        y = rand(ω)
+        val = x^2 + y^2 < 1 ? 4 : 0
+        (;val)
+    end
+
+    @test mean(πapprox.val) ≈ π atol=0.1
+
 end
